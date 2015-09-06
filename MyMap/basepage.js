45e45248b654be62c2dfaaa1bb535b320759ce57
csq12345 <aqing.123@163.com>
2015-09-06 23:51:02 +0800

    12321

---
+++ MyMap/basepage.js
@@ -0,0 +1,26 @@
+/**
+ * Created by nn on 2015/9/6.
+ */
+
+//创建整个画板的图片块
+function AddBlock(){
+
+}
+
+//画板上按下鼠标
+function CanvasOnMouseDown()
+{
+
+}
+
+//画板上抬起鼠标
+function CanvasOnMouseUp()
+{
+
+}
+
+//画板上移动鼠标
+function CanvasOnMouseMove()
+{
+
+}
